574b86f96998032e682b158a2dc63032b354f573
somya21 <somya1667@gmail.com>
2023-08-05 18:59:16 +0530

    Home component & API Call

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/movielisting";
+import movieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/MovieApiKey";
 
-const home = () => {
+const Home = () => {
+    useEffect(() => {
+        const movieText = "Harry";
+        const fetchMovies= async () => {
+            const response = await movieApi
+            .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+            .catch((err) => {
+                console.log("Err:", err);
+            });
+            console.log( "THE reponse from API", response);
+        };
+
+        fetchMovies();
+    }, []); //empty dependency added
+
     return (
-        <div>Home</div>
+        <div>
+        <div className="banner-img"></div>
+        <MovieListing />
+        </div>
     );
 };
 
-export default home;
+export default Home;
